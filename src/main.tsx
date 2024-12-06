8c3ae4d31f6274bc3cb7675ecd5b41888e23f0fc
Max Trewhitt <max.trewhitt@osolo.se>
2024-12-06 13:28:25 +0100

    refactor: update component styles and structure, implement path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import './assets/styles/index.scss';
-import App from './components/App/App.tsx';
+import '@assets/styles/index.scss';
+import Home from '@pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,13 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
 });
