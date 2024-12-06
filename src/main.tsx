e8442c34293f559717aa9b03e461b52d2c976fdf
Max Trewhitt <max.trewhitt@osolo.se>
2024-12-06 12:58:29 +0100

    refactor: reorganize component structure and update asset imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import './index.scss';
-import App from './App.tsx';
+import './assets/styles/index.scss';
+import App from './components/App/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
